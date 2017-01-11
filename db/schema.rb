ed888b9c4684611e9c2615c2b57e9504fac21bbc
Yves Siegrist <Elektron1c97@gmail.com>
2017-01-11 16:09:13 +0100

    use postgres as database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161101140918) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "token"
